e1a4bf058c5194da99e01ff7e9cc918c1bba5c5a
Luke LeFevers <luke.lefevers@gmail.com>
2026-06-02 06:53:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,25 @@ tabs.grid(row=0, column=0)
 oscdimg_exec_label = ttk.Label(tab_general, text="OSCDIMG executable:")
 oscdimg_exec_label.grid(row=0, column=0)
 
+oscdimg_exec = tk.StringVar()
+oscdimg_exec_input = ttk.Entry(tab_general, textvariable=oscdimg_exec)
+oscdimg_exec_input.grid(row=0, column=1)
+
+def open_filedialog_oe():
+    oscdimg_exec.set(filedialog.askopenfilename(filetypes=(("Executables", "*.exe"), ("All Files", "*.*"))))
+oscdimg_exec_browse = ttk.Button(tab_general, text="Browse...", command=open_filedialog_oe)
+oscdimg_exec_browse.grid(row=0, column=2)
+
+input_folder_label = ttk.Label(tab_general, text="Input folder:")
+input_folder_label.grid(row=1, column=0)
+
+input_folder = tk.StringVar()
+input_folder_input = ttk.Entry(tab_general, textvariable=input_folder)
+input_folder_input.grid(row=1, column=1)
+
+def open_filedialog_if():
+    input_folder.set(filedialog.askdirectory())
+input_folder_browse = ttk.Button(tab_general, text="Browse...", command=open_filedialog_if)
+input_folder_browse.grid(row=1, column=2)
+
 window.mainloop()
